37d7189340dcfe64fc063318271242b359a67d2a
Jardel Anton <jardellanton@gmail.com>
2015-04-12 08:09:26 -0300

    implementação parcial de numero

--- Lerarq/Interpretador.java
+++ Lerarq/Interpretador.java
@@ -6,30 +6,51 @@ class Interpretador{
 		String fac;
 		Variavel var;
 		String op;
-		Double vlr=0.0;
+		String ig[]=new String[2];
+		Double vlr;
 		int i=0;
 		while(cm.ordem[i]!=null){// && !cm.ordem[i].equals(""))
-
+		vlr=0.0;
 			fac=cm.ordem[i];
-			
 			switch(fac){
-				case("int"):{
+				case("int"):{// no caso da criação das variaveis
 					i++;					
 					op=cm.ordem[i];
 					i++;
 					
-					if(cm.ordem[i].equals("=")){
+					if(cm.ordem[i].equals("=")){ // o codigo está escrito com espaços. ex: int abc = 3;
 						i++;
 						fac=cm.ordem[i];
 						if(fac.indexOf(';')!=-1){//fim de linha
 							StringTokenizer tokens = new StringTokenizer(fac,";");
 							fac=tokens.nextToken();
-							vlr=Double.parseDouble(fac);
+							vlr=Double.parseDouble(fac);// converte a string em double
 						}else{
 							vlr=Double.parseDouble(fac);
 						}
-			
-					}else{
+					}/*else if(op.indexOf("=")!=-1){// verifica se o igual está na string ex: int abc=3;  
+						fac=cm.ordem[i-1];
+						StringTokenizer tokens = new StringTokenizer(fac,"=");
+						int x=0;
+						while(tokens.hasMoreTokens()){
+							ig[x]=tokens.nextToken();
+							x++;
+						}
+						op=ig[0];
+						System.out.println(ig[0]+ig[1]+cm.ordem[i-1]+cm.ordem[i]);
+						fac=ig[1];
+						if(fac.indexOf(';')!=-1){//fim de linha
+							StringTokenizer tok = new StringTokenizer(fac,";");
+							fac=tok.nextToken();
+							vlr=Double.parseDouble(fac);// converte a string em double
+						}else{
+							vlr=Double.parseDouble(fac);
+						}
+						//vlr=Double.parseDouble(ig[1]);
+
+
+					}*/
+					else{ // é o caso onde não há atribuição. ex: int abc; o valor da var = 0.0
 						StringTokenizer tokens = new StringTokenizer(op,";");
 						if(tokens.hasMoreTokens()){
 							op=tokens.nextToken();
@@ -39,11 +60,32 @@ class Interpretador{
 
 					var=m.getVariavel(op);
 					System.out.println(var.getNome()+" "+var.getValor());
+					break;
 				}default:
-					System.out.println("\t"+i+"\n");	
+					System.out.println(cm.ordem[i]);
+				/*
+				case("func"){// caso de Criação de um escopo de função.
+					break;
+				}
+				case("endfunc"){
+					break;
+				}
+				case("if"){//controle de fluxo if
+					break;
+				}
+				case("endif"){
+					break;
+				}
+				case("loop"){
+					break;
+				}
+				case("endloop"){
+					break;
+				}*/
 			}
+
 			i++;
 
 		}
 	}
-}
+}	
